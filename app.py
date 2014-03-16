12c4f03f54f23a97999bb1c7e78fd3b969979442
Zane Sterling <zane.sterling@gmail.com>
2014-03-16 17:07:50 -0400

    Assemble standard code structure

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return "<center><h1>Asdf</h1></center>"
+	return render_template("home.html")
 
 if __name__ == "__main__":
 	app.run("0.0.0.0", debug=True)
