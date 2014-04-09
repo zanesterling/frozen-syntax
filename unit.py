bc65dd7fc987e8da1d62af254be44228d8b1a888
= <=>
2014-04-09 02:00:09 -0400

    Create distance and collision detection functions in unit

--- unit.py
+++ unit.py
@@ -1,11 +1,12 @@
 import math
 
 class Unit:
-    def __init__(self, x, y, theta):
+    def __init__(self, x, y, theta, radius):
         self.x = x
         self.y = y
         self.theta = theta
         self.speed = 0
+        self.radius = radius
 
     def singleStep(self):
         self.x += self.speed * math.cos(self.theta)
@@ -14,3 +15,12 @@ class Unit:
     def unstep():
         self.x -= self.speed * math.cos(self.theta)
         self.y -= self.speed * math.sin(self.theta)
+
+    def get_quadrance(self.unit):
+        return (self.x - unit.x)**2 - (self.y - unit.y)**2
+
+    def get_distance(self, unit):
+        return math.sqrt(self.get_quadrance(unit))
+
+    def check_collision(self, unit):
+        return self.get_quadrance(unit) > (self.radius + other.radius)**2
