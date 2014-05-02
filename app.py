ae71520fc45028f946e31c8ce085489d872cfac8
Josh Hofing <joshhofing@gmail.com>
2014-05-02 15:50:06 -0400

    Create basic page structure

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.secret_key = "blerp derp"
 
 @app.route('/')
 def home():
-	return render_template("home.html")
+    return render_template("home.html")
 
 if __name__ == "__main__":
 	app.run("0.0.0.0", debug=True)
