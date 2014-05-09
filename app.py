18981fcc0d0b70b77ca3271ba87b54aa437481cb
Josh Hofing <joshhofing@gmail.com>
2014-05-09 12:10:02 -0400

    Add geocities mode

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, session, redirect, url_for, flash
 
 app = Flask(__name__)
 app.secret_key = "blerp derp"
@@ -37,6 +37,17 @@ def versus():
 @app.route('/learn/')
 def learn():
     return render_template("learn.html")
+
+@app.route('/easter/geocities/<value>')
+def geocities(value):
+    if value == "on":
+        session["geocities"] = True
+        flash("Geocities mode activated!")
+    else:
+        if "geocities" in session:
+            session.pop("geocities")
+            flash("Geocities mode deactivated!")
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
 	app.run("0.0.0.0", debug=True)
