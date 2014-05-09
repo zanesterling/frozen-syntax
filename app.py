45e797a11de3df78242e5e2a942487ea52ad3b58
Josh Hofing <joshhofing@gmail.com>
2014-05-09 12:13:25 -0400

    Merge conflicting imports

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for, flash
+from flask import Flask, render_template, session, request, redirect, url_for, flash
 
 app = Flask(__name__)
 app.secret_key = "blerp derp"
