4575ff5096684ba804ef2da39cafc197a49dcc98
Sebastian Conybeare <sebmathguy@gmail.com>
2014-05-19 15:21:43 -0400

    Fix singleStep(self)

--- unit.py
+++ unit.py
@@ -1,20 +1,26 @@
 import math
 
 class Unit:
-    def __init__(self, x, y, heading, radius, speed):
+    def __init__(self, x, y, heading, radius, max_speed):
         self.x = x
         self.y = y
-        self.speed = speed
+        self.max_speed = max_speed
+        self.speed = 0.0
         self.radius = radius
         self.color = 'green'
-        self.heading = 0
+        self.heading = 0.0
         self.destination = (self.x, self.y)
         self.target = None
         self.hit_wall = False
 
     def singleStep(self):
-        self.x += self.speed * math.cos(self.heading)
-        self.y += self.speed * math.sin(self.heading)
+        #If they are far from their destination, go toward it
+        if (self.x - self.destination[0])**2 + (self.y - self.destination[1])**2 > max_speed**2:
+            self.x += self.speed * math.cos(self.heading)
+            self.y += self.speed * math.sin(self.heading)
+        else: #Otherwise, just put them there
+            self.speed = math.sqrt((self.x - self.destination[0])**2 + (self.y - self.destination[1])**2)
+            (self.x, self.y) = self.destination
 
     def unStep():
         self.x -= self.speed * math.cos(self.heading)
