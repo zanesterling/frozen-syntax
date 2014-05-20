f189843313dfb9670a56ce91fb6e01f3bec21b94
Sebastian Conybeare <sebmathguy@gmail.com>
2014-05-20 12:05:41 -0400

    Update the world test

--- world_test.py
+++ world_test.py
@@ -1,8 +1,13 @@
 import world
 import unit
+import math
 
 w = world.World(2)
-u = unit.Unit(0, 0, 0, 1, 1)
+u = unit.Unit(0, 0, 0, 1, .1)
 w.addUnit(u, 0, 0)
-w.go(0, 0, 1, 2)
-w.runStep()
+u = unit.Unit(20, 0, math.pi, 1, .1)
+w.addUnit(u, 1, 0)
+w.go(0, 0, 20, 0)
+w.go(0, 1, 0, 0) #we set units 0 and 1 on a collision course with one another
+for i in xrange(200):
+    w.runStep()
