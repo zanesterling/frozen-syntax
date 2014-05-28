deff678bcc9b480b6213b897a4d3a916a5393dbe
Sebastian Conybeare <sebmathguy@gmail.com>
2014-05-28 16:07:12 -0400

    Create a new file "event.py" with an event class, to encapsulate all of the event stuff.

--- event.py
+++ event.py
@@ -10,4 +10,41 @@ class event:
             self.event_deque.append([])
         #And then add the new event to that list.
         self.event_deque[new_event['timestamp']].append(new_event)
-            
+
+    def actor_seen(self, time, unitID, x, y, player, actor_type):
+        return {'timestamp' : time,
+                'type' : 'ActorSeen',
+                'data' : {'id' : unitID,
+                          'x' : x,
+                          'y' : y,
+                          'team' : player,
+                          'type' : actor_type
+                          }
+                }
+
+    def actor_hidden(self, time, unitID):
+        return {'timestamp' : time,
+                'type' : 'ActorHidden',
+                'data' : {'id' : unitID
+                          }
+                }
+
+    def actor_velocity_change(self, time, unitID, x, y, vx, vy):
+        return {'timestamp' : time,
+                'type' : 'ActorVelocityChange',
+                'data' : {'id' : unitID,
+                          'x' : x,
+                          'y' : y,
+                          'vx' : vx,
+                          'vy' : vy
+                          }
+                }
+
+    def actor_position_update(self, time, unitID, x, y):
+        return {'timestamp' : time,
+                'type' : 'ActorPositionUpdate',
+                'data' : {'id' : unitID,
+                          'x' : x,
+                          'y': y
+                          }
+                }
