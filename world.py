274bcd64e6270daab149ca58690c1c07cde9db82
Josh Hofing <joshhofing@gmail.com>
2014-05-31 17:42:45 -0400

    Switch world to use lists for walls and units.

--- world.py
+++ world.py
@@ -7,39 +7,35 @@ import wall
 
 class World(object):
     def __init__(self, width, height):
-        self.units = {} # Mapping of id -> unit
-        self.walls = {} # Mapping of id -> wall
+        self.units = []
+        self.walls = []
         self.history = event.History()
         self.timestamp = 0
 
     def add_unit(self, player, x, y, radius):
         """ Add a unit to the list of units, giving it an id as appropriate.
         Inform the unit of this world, it's id, and generate an event to inform the client
-        Returns the assigned id """
-        # Find an untaken id, assign it to the unit
-        for i in xrange(len(self.units)+1):
-            if not i in self.units:
-                self.units[i] = unit.Unit(self, player, x, y, radius, i)
-                self.history.actor_spawned(self.units[i])
-                return self.units[i]
+        Returns the assigned new unit """
+        id = len(self.units)
+        self.units.append(unit.Unit(self, player, x, y, radius, id))
+        self.history.actor_spawned(self.units[id])
+        return self.units[id]
 
     def add_wall(self, x, y, width, height):
         """ Add a wall to the list of walls, giving it an appropriate id.
         Inform the client of this new wall
         Return assigned id"""
-        for i in xrange(len(self.walls)+1):
-            if not i in self.walls:
-                self.walls[i] = wall.Wall(self, x, y, width, height, i)
-                self.history.wall_added(wall)
-                return wall
+        id = len(self.walls)
+        self.walls.append(wall.Wall(self, x, y, width, height, id))
+        self.history.wall_added(self.walls[id])
+        return self.walls[id]
 
     def step(self):
         """ Step all the units forward one timestep """
         self.timestamp += 1
         factor = 1 # Number of sub-steps we need for numerical integration
         for i in xrange(factor):
-            for unitID in self.units:
-                unit = self.units[unitID]
+            for unit in self.units:
                 if not unit.dead:
                     unit.x += unit.vx / factor
                     unit.y += unit.vy / factor
@@ -49,16 +45,12 @@ class World(object):
     def handle_collisions(self):
         """ Check for collisions between each pair of unit, and if they exist, resolve them.
         Then, check for collisions between units and walls, and if they exist, resolve them. """
-        for (first,second) in combinations(self.units, 2):
-            unit1 = self.units[first]
-            unit2 = self.units[second]
+        for (unit1,unit2) in combinations(self.units, 2):
             if unit1.is_colliding_with(unit2):
                 self.resolve_unit_collision(unit1, unit2)
         # unit -> wall collisions
-        for unitid in self.units:
-            unit = self.units[unitid]
-            for wallid in self.walls:
-                wall = self.walls[wallid]
+        for unit in self.units:
+            for wall in self.walls:
                 if wall.is_colliding_with(unit):
                     self.resolve_wall_collision(wall, unit)
 
@@ -99,7 +91,7 @@ class World(object):
     
     def move_unit(self, unit_id, heading, speed):
         """ Callback to make a unit move from lisp code """
-        if unit_id in self.units:
-            self.units[unit_id].heading = heading
-            self.units[unit_id].speed = speed
+        if unit in self.units:
+            unit.heading = heading
+            unit.speed = speed
         return
