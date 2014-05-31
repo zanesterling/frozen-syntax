a09baeb7a7c03d9170eae6f6b161762e7d4bd6e9
Josh Hofing <joshhofing@gmail.com>
2014-05-31 14:36:09 -0400

    Switch callbacks to take the player's id at call-time, instead of at creation time

--- callbacks.py
+++ callbacks.py
@@ -18,13 +18,12 @@ class Callbacks(object):
         Returns dict of canonical id -> unit """
         return self.units[player]
 
-    def callbacks(self, player):
-        """ Returns dict of lisp function-name -> function,
-        for the specified player.
+    def callbacks(self):
+        """ Returns dict of lisp function-name -> function.
         These functions takes a canonical id as the first argument,
         then specific arguments to the function """
 
-        def move(unit_id, heading, speed):
+        def move(player, unit_id, heading, speed):
             """ Callback to move the specified unit in the specified way """
             if unit_id in self.units[player]:
                 self.units[player][unit_id].heading = heading
@@ -32,15 +31,15 @@ class Callbacks(object):
             else:
                 raise ValueError("Unit with unit_id",unit_id,"does not exist")
         
-        def aim(unit_id, angle):
+        def aim(player, unit_id, angle):
             """ Callback to aim a unit's turret at the specified angle """
             pass
 
-        def shoot(unit_id):
+        def shoot(player, unit_id):
             """ Callback to make a unit shoot """
             pass
 
-        def units_seen(unit_id):
+        def units_seen(player, unit_id):
             """ Callback to return a lisp list of all the units we can see """
             pass
 
