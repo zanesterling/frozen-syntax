9f420fa70dbae6e35cc4e5b47977878201251f3f
Joshua Chin <joshuarchin@gmail.com>
2014-06-01 22:23:22 -0400

    Update interface.py
    fixed syntax error

--- interface.py
+++ interface.py
@@ -1,4 +1,4 @@
 from jcli import jcli
 
 def interpret(srces, maxSteps, reductionsPerStep, step, callbacks=None, builtins=None):
-    return jcli.jcli_exec(srcs, maxSteps, step, reductionsPerSetp, callbacks, builtins)
+    return jcli.jcli_exec(srces, maxSteps, step, reductionsPerStep, callbacks, builtins)
