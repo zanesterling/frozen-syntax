dd504e5da76bab783a08344e8183fc3d84ec06c7
Josh <joshhofing@gmail.com>
2014-06-02 12:18:19 -0400

    Spawn bullets before setting their trajectory

--- bullet.py
+++ bullet.py
@@ -9,8 +9,8 @@ default_bullet_speed = 100
 class Bullet(actor.Actor):
     def __init__(self, world, player, x, y, heading, speed):
         super(Bullet, self).__init__(world, player, x, y)
-        self.heading = heading
-        self.speed = speed
         self.bulletID = len(self.world.bullets)
         self.world.bullets.append(self)
         self.world.history.actor_spawned(self)
+        self.heading = heading
+        self.speed = speed
